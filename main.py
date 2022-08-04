484501d9adc39640931694398c97db50406353b2
Aayoung Adele KIM <83590357+kimmaa@users.noreply.github.com>
2022-08-04 16:37:35 +0900

    streamlit 으로 만드는 파이썬

---
+++ main.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+view = [300,200,100,500]
+
+show_raw=st.checkbox('show raw data')
+if show_raw ==True:
+    st.write('# raw data')
+    view
+
+show_raw
+st.write('# raw data')
+st.write('# table')
+st.table(view)
+st.write('# bar graph')
+st.bar_chart(view)
